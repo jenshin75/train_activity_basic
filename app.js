d7fc116ce286648f18b963befe59d5ccd7961728
jenshin75 <49443598+jenshin75@users.noreply.github.com>
2019-06-06 18:36:25 -0500

    updated 06/06/19 6:36pm

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ firebase.initializeApp(firebaseConfig);
 
 var database = firebase.database();
 
-$("#submit").on("click", function(){
+$("#submit").on("click", function () {
 
   //saves input values to variables
   var trainInput = $("#train").val().trim();
@@ -27,111 +27,112 @@ $("#submit").on("click", function(){
     train: trainInput,
     destination: destinationInput,
     frequency: frequencyInput,
-    firstTrain: firstTrainInput    
+    firstTrain: firstTrainInput
   })
 
 })
 
 //display data from initial load
-database.ref().on("child_added",function(snapshot){
+database.ref().on("child_added", function (snapshot) {
 
- var train=snapshot.val().train;
- var destination=snapshot.val().destination;
- var firstTrain=snapshot.val().firstTrain;
- var frequency=snapshot.val().frequency;
+  var train = snapshot.val().train;
+  var destination = snapshot.val().destination;
+  var firstTrain = snapshot.val().firstTrain;
+  var frequency = snapshot.val().frequency;
 
-//  var months=10;
-//  var totalBill= months * monthlyRate;
+  //  var months=10;
+  //  var totalBill= months * monthlyRate;
 
- // Assume the following situations.
+  // Assume the following situations.
 
-    // (TEST 1)
-    // First Train of the Day is 3:00 AM
-    // Assume Train comes every 3 minutes.
-    // Assume the current time is 3:16 AM....
-    // What time would the next train be...? (Use your brain first)
-    // It would be 3:18 -- 2 minutes away
+  // (TEST 1)
+  // First Train of the Day is 3:00 AM
+  // Assume Train comes every 3 minutes.
+  // Assume the current time is 3:16 AM....
+  // What time would the next train be...? (Use your brain first)
+  // It would be 3:18 -- 2 minutes away
 
-    // (TEST 2)
-    // First Train of the Day is 3:00 AM
-    // Assume Train comes every 7 minutes.
-    // Assume the current time is 3:16 AM....
-    // What time would the next train be...? (Use your brain first)
-    // It would be 3:21 -- 5 minutes away
+  // (TEST 2)
+  // First Train of the Day is 3:00 AM
+  // Assume Train comes every 7 minutes.
+  // Assume the current time is 3:16 AM....
+  // What time would the next train be...? (Use your brain first)
+  // It would be 3:21 -- 5 minutes away
 
 
-    // ==========================================================
+  // ==========================================================
 
-    // Solved Mathematically
-    // Test case 1:
-    // 16 - 00 = 16
-    // 16 % 3 = 1 (Modulus is the remainder)
-    // 3 - 1 = 2 minutes away
-    // 2 + 3:16 = 3:18
+  // Solved Mathematically
+  // Test case 1:
+  // 16 - 00 = 16
+  // 16 % 3 = 1 (Modulus is the remainder)
+  // 3 - 1 = 2 minutes away
+  // 2 + 3:16 = 3:18
 
-    // Solved Mathematically
-    // Test case 2:
-    // 16 - 00 = 16
-    // 16 % 7 = 2 (Modulus is the remainder)
-    // 7 - 2 = 5 minutes away
-    // 5 + 3:16 = 3:21
+  // Solved Mathematically
+  // Test case 2:
+  // 16 - 00 = 16
+  // 16 % 7 = 2 (Modulus is the remainder)
+  // 7 - 2 = 5 minutes away
+  // 5 + 3:16 = 3:21
 
-    //===
+  
+  // Assumptions
+  var tFrequency = frequency;
+  console.log("tFrequency:" + frequency);
+  
+  // // Time is 3:30 AM
+  var firstTime = firstTrain;
+  console.log("firstTrain:" + firstTrain);
 
-    // // Assumptions
-    // var tFrequency = $("frequency");
+  //==
 
-    // // Time is 3:30 AM
-    // var firstTime = $("firstTrain");
-   
-    // //==
+  // First Time (pushed back 1 year to make sure it comes before current time)
+  var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+  console.log(firstTimeConverted);
 
-    // // First Time (pushed back 1 year to make sure it comes before current time)
-    // var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
-    // console.log(firstTimeConverted);
+  // Current Time
+  var currentTime = moment();
+  console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
 
-    // // Current Time
-    // var currentTime = moment();
-    // console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+  // Difference between the times
+  var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+  console.log("DIFFERENCE IN TIME: " + diffTime);
 
-    // // Difference between the times
-    // var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-    // console.log("DIFFERENCE IN TIME: " + diffTime);
+  // Time apart (remainder)
+  var tRemainder = diffTime % tFrequency;
+     console.log(tRemainder);
 
-    // // Time apart (remainder)
-    // var tRemainder = diffTime % tFrequency;
-    //    console.log(tRemainder);
+  // Minute Until Train
+  var tMinutesTillTrain = tFrequency - tRemainder;
+  console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
 
-    // // Minute Until Train
-    // var tMinutesTillTrain = tFrequency - tRemainder;
-    // console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+  // Next Train
+  var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+  console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
 
-    // // Next Train
-    // var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-    // console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+  var tr = $("<tr>");
 
-var tr=$("<tr>");
-   
-var td1=$("<td>")
-td1.text(train)
-tr.append(td1)
+  var td1 = $("<td>")
+  td1.text(train)
+  tr.append(td1)
 
-var td2=$("<td>")
-td2.text(destination)
-tr.append(td2)
+  var td2 = $("<td>")
+  td2.text(destination)
+  tr.append(td2)
 
-var td3=$("<td>")
-td3.text(frequency)
-tr.append(td3)
+  var td3 = $("<td>")
+  td3.text(frequency)
+  tr.append(td3)
 
-// var td4=$("<td>")
-// td4.text(nextTrain)
-// tr.append(td4)
-     
-// var td5=$("<td>")
-// td5.text(tMinutesTillTrainfirstTrain)
-// tr.append(td5)
+  // var td4=$("<td>")
+  // td4.text(nextTrain)
+  // tr.append(td4)
 
-$("tbody").append(tr);
+  // var td5=$("<td>")
+  // td5.text(tMinutesTillTrainfirstTrain)
+  // tr.append(td5)
+
+  $("tbody").append(tr);
 
 })
